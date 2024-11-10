80b188f353b27c0cf606d8e1bc06db70cb2c3a12
MR211604 <elmastrollxd34@gmail.com>
2024-11-09 18:48:05 -0600

    First commit

---
+++ src/main/java/org/example/springbootcartapi/SpringBootCartApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootcartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCartApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springbootcartapi/SpringBootCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootcartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
